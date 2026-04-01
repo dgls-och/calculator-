c594c6f6951ddfb72e5f10e0fae85f5cc75815a9
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-04-01 23:21:29 +0100

    Add 'operate' function to provide a means for unified operation

--- script.js
+++ script.js
@@ -14,6 +14,24 @@ function findMultiple(num1, num2) {
     return num1 * num2;
 }
 
+function operate(operator, firstOperand, secondOperand) {
+    if (operator === "+") {
+        return findSum(firstOperand, secondOperand);
+    }
+
+    if (operator === "-") {
+        return findDifference(firstOperand, secondOperand);
+    }
+
+    if (operator === "*") {
+        return findMultiple(firstOperand, secondOperand);
+    }
+
+    if (operator === "/") {
+        return findQuotient(firstOperand, secondOperand);
+    }
+}
+
 let firstOperand;
 let operator;
 let secondOperand;
