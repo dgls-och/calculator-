f581b187f8d4da1eed1bc9472064e31396666bc2
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-04-01 22:57:18 +0100

    Add functions for basic arithmetic operations

---
+++ script.js
@@ -0,0 +1,16 @@
+function findSum(num1, num2) {
+    return num1 + num2;
+}
+
+function findDifference(num1, num2) {
+    return num1 - num2;
+}
+
+function findQuotient(num1, num2) {
+    return num1 / num2;
+}
+
+function findMultiple(num1, num2) {
+    return num1 * num2;
+}
+
